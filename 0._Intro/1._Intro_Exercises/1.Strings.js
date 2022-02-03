4729098a32da78359c10b00229129237a9c89695
inesluparia <inesluparia@gmail.com>
2022-02-03 13:04:02 +0100

    Intro exercises

---
+++ 0._Intro/1._Intro_Exercises/1.Strings.js
@@ -0,0 +1,52 @@
+// --------------------------------------
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// Exercise 1 - Add numbers from string to float
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+console.log("Exercise 1. ",+numberOne + +numberTwo)
+
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 2 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+console.log("Exercise 2. ",(+numberOne + +numberTwo).toFixed(2))
+
+// --------------------------------------
+// Exercise 3 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+console.log("Exercise 3. ",((one + two + three)/3).toFixed(5))
+
+
+// --------------------------------------
+// Exercise 4 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+console.log("Exercise 4. ", letters.charAt(2))
+
+
+// --------------------------------------
+// Exercise 5 - Replace
+
+const fact = "You are learning javascript!";
+console.log(fact.replace('j', 'J'))
+
+// capitalize the J in Javascript
+
+
+// --------------------------------------
+
+
+
---
+++ 0._Intro/1._Intro_Exercises/2.Objects.js
@@ -0,0 +1,45 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const alienMessage = {
+	message: "Hello, earthling! I bring peace."
+};
+
+// Log the message 
+console.log(alienMessage.message)
+
+// --------------------------------------
+// Exercise 2 - Defining an object. 
+
+// Create an object that has your name and age. 
+const obj = {
+	name: "Ines",
+	age: 22
+}
+
+
+// --------------------------------------
+// Exercise 3 - Add a property 
+
+const stackOverflow = {};
+stackOverflow.isAllowed = true
+console.log(stackOverflow.isAllowed)
+// make a rule called isAllowed and let the value be true
+
+// --------------------------------------
+// Exercise 4 - Remove a property 
+
+const thisSong = {
+	description: "The best song in the world."
+}
+
+// remove the property "description" and add a property called "about" that should say "Just a tribute."
+delete thisSong.description
+thisSong.about = "Just a tribute."
+console.log(JSON.stringify(thisSong))
+
+// --------------------------------------
+
+
